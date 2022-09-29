1626104ac81d39d6efd2b8850f23d2dff2960e45
valipsiree1208 <114668759+valipsiree1208@users.noreply.github.com>
2022-09-29 18:49:55 +0530

    Add files via upload

---
+++ app4/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def sireesha(request):
+    return HttpResponse("<h1><b>Sireesha jsipders<b><h1>")
+
+
+
+# Create your views here.
